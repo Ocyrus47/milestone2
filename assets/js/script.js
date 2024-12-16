b3d1c0f9e2f70bccad8fb9f2f232986643545ca3
Ocyrus47 <omomshanti_47@hotmail.com>
2024-12-16 00:40:55 +0000

    Add style to recipe-container and recipe div to align with responsive screens. Add h2 to recipeContainer and assign empty to return reciveDiv

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,18 @@ const recipeContainer = document.querySelector('.recipe-container');
  * Function to fetch recipes
  * via API.
  */
-const fetchRecipes = async (query) => {                                                        // async function to allow await
-    const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`); // await used to load full data before return
+const fetchRecipes = async (query) => {                                                         // async function to allow await
+    recipeContainer.innerHTML = "<h2>Fetching Recipes...</h2>";                                 
+    const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);  // await used to load full data before return
     const response = await data.json();                                                         // await used to load full data before return
     
+    recipeContainer.innerHTML = "";
+
     response.meals.forEach(meal => {
-        const recipeDiv = document.createElement('div');
+        const recipeDiv = document.createElement('div');                                        // create DEV element for holding menu items and asigning class "recipe" to it
         recipeDiv.classList.add('recipe');                                                      // strMealThumb = image stored in API
         recipeDiv.innerHTML =`
-            <img src ="${meal.strMealThumb}">  
+            <img src ="${meal.strMealThumb}">                                                   
             <h3>${meal.strMeal}</h3>
             <p>${meal.strArea}</p>
             <p>${meal.strCategory}</p>                                          
